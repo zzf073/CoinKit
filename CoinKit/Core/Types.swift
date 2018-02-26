d3a6bbfb2689d906b43a3237e21c1096545a3672
Dmitry <d.overchuk@titanium.im>
2018-02-26 14:03:46 +0300

    Address validation

--- CoinKit/Core/Types.swift
+++ CoinKit/Core/Types.swift
@@ -6,8 +6,18 @@
 //
 
 import Foundation
+import CoreBitcoin
+import ethers
 
 public enum WalletType:Int {
     case BTC = 0
     case ETH = 60
+    
+    public func validateAddress(_ addressString:String) -> Bool {
+        
+        switch self {
+        case .BTC: return BTCAddress.init(string: addressString) != nil
+        case .ETH: return Address.init(string: addressString) != nil
+        }
+    }
 }
